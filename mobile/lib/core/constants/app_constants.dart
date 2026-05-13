02a95f0887f38c7ba5e119735b52d99a49233f99
Muhammadqodir <mirzaolimovinfinity@gmail.com>
2026-05-13 21:44:09 +0500

    fix: update baseUrl to Railway production URL

--- mobile/lib/core/constants/app_constants.dart
+++ mobile/lib/core/constants/app_constants.dart
@@ -1,5 +1,5 @@
 class AppConstants {
-  static const String baseUrl = 'https://tacky-navy-flatly.ngrok-free.dev';
+  static const String baseUrl = 'https://my-application-production-9888.up.railway.app';
   static const String appName = 'Master Scan';
   static const int connectTimeout = 15000;
   static const int receiveTimeout = 30000;
